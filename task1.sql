2bbc37c6a809ea8e8bc972e4dcc0b6c36a8de9f3
unknown <smruthibs.xworkz@gmail.com>
2022-10-16 13:16:04 +0530

    push1

---
+++ task1.sql
@@ -0,0 +1,14 @@
+use  task;
+  CREATE TABLE book(title varchar(50),author varchar(50),edition int,no_of_pages int,year_of_publish bigint,no_0f_copies int,no_of_readers int,price int,publication varchar(50),no_of_chapter int);
+select *from book;
+ INSERT INTO book values('karvalo ','Poornachandra Tejaswi',9,150,1990,20,1000,114,'Pustaka Prakashana',1);
+ INSERT INTO book values(' Kanooru Heggadithi','Kuvempu',4,591,1990,100,1000,375,' Udaya Ravi Prakaashana',1);
+ INSERT INTO book values('Madhuvana','MK indira',1,216,1977,100,1000,114,'Prakashana',2);
+ INSERT INTO book values('Chidambara Rahasya','Poornachandra Tejaswi',5,220,1985,30,1000,215,'Pustaka Prakashana',1);
+ INSERT INTO book values('Rudraprayaagada Bhayaanaka Narabhakshaka','Poornachandra Tejaswi',2,180,1995,40,1000,127,'Pustaka Prakashana',1);
+ INSERT INTO book values('Mookajjiya Kanasugalu','Kota Shivarama Karanth',8,272,1968,50,1000,200,' sapna book house',1);
+ INSERT INTO book values('Jugaari Cross','Poornachandra Tejaswi',2,200,1994,209,1000,125,'Pustaka Prakashana',1);
+ INSERT INTO book values('Grihabhanga','S.L. Bhyrappa',1,113,1970,90,1000,200,'Sahithya Bhandara',1);
+ INSERT INTO book values('Kiragoorina Gayyaaligalu','Poornachandra Tejaswi',1,150,1991,90,1000,114,'Pustaka Prakashana',1);
+ INSERT INTO book values('Chomana Dud','Kota Shivarama Karanth',9,150,1990,20,1000,114,'Pustaka Prakashana',1);
+ select *from book;
